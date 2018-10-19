881331b78111a7e215622ae34e85d8e22f92796c
mag0029 <magali.giraud29@gmail.com>
2018-10-19 17:09:59 +0200

    update

--- src/main/java/com/mgiraud/bataillenavale/entities/manager/Jeu.java
+++ src/main/java/com/mgiraud/bataillenavale/entities/manager/Jeu.java
@@ -18,19 +18,19 @@ public class Jeu {
 
 	public void initialiseBateaux(Joueur joueurInit,Carte map, int nbCorvette, int nbDestroyer, int nbCroiseur, int nbPorteAvion) {
 		char id = 'a';
-		for (int i = 1; i == nbCorvette; i++) {
+		for (int i = 0; i < nbCorvette; i++) {
 			joueurInit.ajoutBateau(new Corvette(" "+id+" "), map);
 			id++;
 		}
-		for (int i = 1; i == nbDestroyer; i++) {
+		for (int i = 0; i < nbDestroyer; i++) {
 			joueurInit.ajoutBateau(new Destroyer(" "+id+" "), map);
 			id++;
 		}
-		for (int i = 1; i == nbCroiseur; i++) {
+		for (int i = 0; i < nbCroiseur; i++) {
 			joueurInit.ajoutBateau(new Croiseur(" "+id+" "), map);
 			id++;
 		}
-		for (int i = 1; i == nbPorteAvion; i++) {
+		for (int i = 0; i < nbPorteAvion; i++) {
 			joueurInit.ajoutBateau(new PorteAvion(" "+id+" "), map);
 			id++;
 		}
